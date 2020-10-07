987fce7a2fd600c62d2e28f49283055690b7d4f8
Pavlo Andrushkiv <monday13081@gmail.com>
2020-10-07 22:03:03 +0300

    learned something new

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = False
+# db = SQLAlchemy(app)
+#
+#
+# class Article(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(30), nullable=False)
+#     text = db.Column(db.Text, nullable=False)
+#     date = db.Column(db.DateTime, default=datetime.utcnow)
+#
+#     def __repr__(self):
+#         return '<Article %r>' % self.id
 
 
 @app.route('/')
@@ -12,6 +27,11 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/create-post')
+def create_article():
+    return render_template('create_post.html')
 
 
 @app.route('/post/<int:post_id>')
