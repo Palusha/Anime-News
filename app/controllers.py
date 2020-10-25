c5c7eec1456bd9735ff79d39ad6e3cf47b2c31e9
v0kan <axineop@gmail.com>
2020-10-25 09:51:13 +0100

    Added flask.flash, added password hashing, improved users' profiles

--- app/controllers.py
+++ app/controllers.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, \
+    redirect, session, flash
 from flask_login import login_user, login_required, \
     logout_user, current_user
 from datetime import datetime
@@ -38,16 +39,17 @@ def signup():
 
         user = User.query.filter_by(username=lgn).first()
         if user:
-            print("Пользователь", lgn, "уже существует!")
-            return redirect('/login')
+            flash("Користувач з іменем " + str(lgn) + " вже існує.")
+            return redirect('/signup')
         if passw_one == passw_two:
-            new_user = User(username=lgn, password=passw_one, avatar=User.create_avatar(lgn, 512))
+            new_user = User(username=lgn, avatar=User.create_avatar(lgn, 512))
+            new_user.set_password(passw_one)
             db.session.add(new_user)  
             db.session.commit()
-            print('Регистрация прошла успешно!')
+            flash('Акаунт створений!')
             return redirect('/login', code=302)
         else:
-            print("Вы ввели разные пароли! Попробуйте ещё раз.")
+            flash("Ви ввели різні паролі. Спробуйте знову.")
             return redirect('/signup')
 
     return render_template('users-auth/signup.html')
@@ -63,28 +65,26 @@ def login():
         passw = request.form.get("password")
 
         user = User.query.filter_by(username=lgn).first()
-        if user:
-            if passw == user.password:
-                print('Вы успешно ввошли в аккаунт!')
-                login_user(user)
-                return redirect('/')
-            else:
-                print('Вы ввели неверный пароль!')
-                return redirect('/login')
-        print("Пользователя", lgn, "не существует!")
-        return redirect('/login')
+        if not user or not user.check_password(passw):
+            flash("Некорректне ім'я користувача або пароль.")
+            return redirect('/login')
+        flash('Ви успішно ввійшли в акаунт!')
+        login_user(user)
+        return redirect('/')
 
     return render_template('users-auth/login.html')
 
-@auth.route('/profile/')
+@auth.route('/profile/<username>')
 @login_required
-def show_profile():
-    return render_template('users-auth/profile.html', user_data=current_user)
+def show_profile(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('users-auth/profile.html', user=user)
 
 @auth.route('/logout')
 @login_required
 def logout():
    logout_user()
+   flash("Ви вийшли з акаунта.")
    return redirect('/')
 
 
@@ -95,11 +95,10 @@ def create_article():
     if request.method == "POST":
         title = request.form.get("title")
         post_info = request.form.get("text")
-        new_post = Article(title=title, text=post_info)
-
+        new_post = Article(title=title, text=post_info, user_id=current_user.id)
         db.session.add(new_post)
         db.session.commit()
-
+        flash("Стаття була успішно створена.")
     return render_template('posts/create-post.html')
 
 
@@ -116,6 +115,7 @@ def delete_article(id):
     db.session.query(Article).filter(Article.id == id).delete()
     db.session.query(Comment).filter(Comment.article_id == id).delete()
     db.session.commit()
+    flash("Стаття була успішно видалена.")
     return redirect("/article-table")
 
 @auth.route('/edit-post/<int:id>', methods=['POST', 'GET'])
@@ -128,21 +128,21 @@ def edit(id):
         new_text = request.form.get("text")
 
         if new_title == article.title and new_text == article.text:
-            print('Вы ничего не изменили!')
+            flash('Ви нічого не змінили!')
             return redirect('/edit-post/' + str(id))
 
         article.title = new_title
         article.text = new_text
+        article.last_update = datetime.now()
         db.session.commit()
-
+        flash("Зміни успішно впроваджені.")
     return render_template('posts/edit-post.html', article=article)
 
 
 @auth.route('/post/<int:id>', methods=['POST', 'GET'])
 def show_post(id):
     article = Article.query.filter_by(id=id).first_or_404()
-    users = User.query.all()
-    return render_template("posts/post.html", article=article, users=users)
+    return render_template("posts/post.html", article=article)
 
 
 @auth.route('/create-comment/<int:id>', methods=['POST', 'GET'])
@@ -161,4 +161,5 @@ def delete_comment(id):
     article = Comment.query.filter_by(id=id).first().article_id
     db.session.query(Comment).filter(Comment.id == id).delete()
     db.session.commit()
+    flash("Коментар був успішно видалений.")
     return redirect('/post/' + str(article))
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db, login_manager
 from flask_login import UserMixin
 from datetime import datetime
 from hashlib import md5
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @login_manager.user_loader
 def load_user(user):
@@ -12,11 +13,19 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), nullable=False, unique=True)
-    password = db.Column(db.String(50), nullable=False)
+    password_hash = db.Column(db.String(50), nullable=False)
     avatar = db.Column(db.String(50), default='default.jpg', nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.DateTime, default=datetime.now())
 
     comments = db.relationship('Comment', backref='user', lazy='dynamic')
+    articles = db.relationship('Article', backref='user', lazy='dynamic')
+
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def create_avatar(unique_attr, size):
         digest = md5(unique_attr.lower().encode('utf-8')).hexdigest()
@@ -30,9 +39,12 @@ class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(30), nullable=False)
     text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.DateTime, default=datetime.now())
+    last_update = db.Column(db.DateTime, default=None)
 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     comments = db.relationship('Comment', backref='article', lazy='dynamic')
+
 
 class Comment(db.Model):
     __bind_key__ = 'comments' # <-- binded key
@@ -40,7 +52,7 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(100), nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.DateTime, default=datetime.now())
 
     article_id = db.Column(db.Integer, db.ForeignKey('article.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
